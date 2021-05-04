3936a46dae8545e9c720de4ca5e4794b21577cd1
QuqqU <rldnd913@naver.com>
2021-05-04 10:35:35 +0900

    Create 3-1-Topological-Sort-DFS.cpp

---
+++ 3-1-Topological-Sort-DFS.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+vector<vector<int>> edge;
+vector<bool> visited;
+vector<int> topology;
+
+void dfs(int curr) {
+	visited[curr] = true;
+	for (int next : edge[curr])
+		if (!visited[next]) dfs(next);
+	topology.push_back(curr);
+}
+
+int main() {
+	int n, m;
+	scanf("%d %d", &n, &m);
+	edge.resize(n + 1);
+	visited.resize(n + 1, false);
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		edge[a].push_back(b);
+	}
+	for (int i = 1; i <= n; i++)
+		if (!visited[i]) dfs(i);
+
+	reverse(topology.begin(), topology.end());
+
+	for (int cursor : topology)
+		printf("%d ", cursor);
+
+	return 0;
+}
